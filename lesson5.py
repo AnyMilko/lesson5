9f93a1c3dde27fd109295bc29ffca2d2093e04b9
AnyMilko <milko.ann@mail.ru>
2024-09-24 19:17:58 +0300

    Add files via upload

---
+++ lesson5.py
@@ -0,0 +1,9 @@
+immutable_var=(1,2.3, True, [3])
+print(immutable_var)
+
+
+mutable_list = ("pen", "pensil", "map")
+print(mutable_list)
+print(mutable_list[0])
+mutable_list[0] = "list" #
+print(mutable_list)
